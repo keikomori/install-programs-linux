f62959ac45c05cf74047b559a2bfff852820a18a
Tatiany Mori <tatianymori@gmail.com>
2021-04-19 09:40:14 -0300

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -82,6 +82,24 @@ sudo snap install telegram-desktop &&
 echo 'Instalando Slack'
 sudo apt-get install slack-desktop -y &&
 
+#instalando postgresql
+# Create the file repository configuration:
+sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list' &&
+
+# Import the repository signing key:
+wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add - &&
+
+# Update the package lists:
+sudo apt-get update &&
+
+# Install the latest version of PostgreSQL.
+# If you want a specific version, use 'postgresql-12' or similar instead of 'postgresql':
+sudo apt-get -y install postgresql-10 &&
+
+#install pycharm
+sudo snap install pycharm-community --classic &&
+
+sudo snap refresh pycharm-community --classic &&
 
 echo "Instalação bem sucedida!"
 echo  .##.....##.#########.##.##.....##.#########..
